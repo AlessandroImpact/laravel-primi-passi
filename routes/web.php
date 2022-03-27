096309c80dec5fb03aeb822954c52bcc0759c32b
AlessandroImpact <terracciano41@gmail.com>
2022-03-27 17:16:28 +0200

    passaggio dati dinamico

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('hello');
+    return view('home');
+});
+
+
+Route::get('/hello', function () {
+    return view('hello', ['title' => 'Hello World!']);
 });
